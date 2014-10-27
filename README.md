b8cfa4b31b4783a200d5b9cd305cac6510cf0853
Andrew Leverentz <buggi22@users.noreply.github.com>
2014-10-27 13:14:09 -0700

    Add another basic example

--- README.md
+++ README.md
@@ -4,6 +4,51 @@ Compute probabilities using Bayesian networks.
 
 This is intended to be a "toy" implementation for educational purposes.  The code is not designed for efficiency and is particularly susceptible to numerical rounding errors.
 
+## Usage
+
+Consider the example outlined [here](http://en.wikipedia.org/wiki/Bayesian_network#Example).  The following code illustrates how to calculate the probability that it is raining, given that the grass is wet.
+
+```
+  private static final double DELTA = 0.000001;
+
+  public void testGrassNetwork() {
+    BayesNetwork network = BayesNetwork.builder()
+        .add(ConditionalDistribution
+            .forVariable("Rain")
+            .setProbability(1/5d, "True")
+            .setProbability(4/5d, "False")
+            .build())
+        .add(ConditionalDistribution
+            .forVariable("Sprinkler")
+            .setParents("Rain")
+            .setProbability(2/5d, "True", "False")
+            .setProbability(3/5d, "False", "False")
+            .setProbability(1/100d, "True", "True")
+            .setProbability(99/100d, "False", "True")
+            .build())
+        .add(ConditionalDistribution
+            .forVariable("GrassWet")
+            .setParents("Sprinkler", "Rain")
+            .setProbability(0d, "True", "False", "False")
+            .setProbability(1d, "False", "False", "False")
+            .setProbability(4/5d, "True", "False", "True")
+            .setProbability(1/5d, "False", "False", "True")
+            .setProbability(9/10d, "True", "True", "False")
+            .setProbability(1/10d, "False", "True", "False")
+            .setProbability(99/100d, "True", "True", "True")
+            .setProbability(1/100d, "False", "True", "True")
+            .build())
+        .build();
+
+    Event queryEvent = varEquals("Rain", "True");
+    Event evidence = varEquals("GrassWet", "True");
+
+    double probability =
+        network.queryProbabilityWithEvidence(queryEvent, evidence);
+    assertEquals(891/2491d, probability, DELTA);
+  }
+```
+
 ## Unit tests
 
 Unit test can be found under the [`test`](/test/com/example/ai/bayes) directory.
--- test/com/example/ai/bayes/ExampleTest.java
+++ test/com/example/ai/bayes/ExampleTest.java
@@ -1,5 +1,9 @@
 package com.example.ai.bayes;
 
+import static com.example.ai.bayes.Event.and;
+import static com.example.ai.bayes.Event.or;
+import static com.example.ai.bayes.Event.varEquals;
+
 import com.google.common.collect.ImmutableList;
 
 import junit.framework.TestCase;
@@ -14,13 +18,15 @@ public class ExampleTest extends TestCase {
    */
   private BayesNetwork getLadyOrTigerNetwork() {
     BayesNetwork.Builder builder = BayesNetwork.builder()
-        .add(ConditionalDistribution.forVariable("InitialChoice")
+        .add(ConditionalDistribution
+            .forVariable("InitialChoice")
             .setProbability(1/3d, "TT")
             .setProbability(1/3d, "TL")
             .setProbability(1/3d, "LL")
             .build());
     for (int i = 1; i <= 3; i++) {
-      builder.add(ConditionalDistribution.forVariable("Reveal" + i)
+      builder.add(ConditionalDistribution
+          .forVariable("Reveal" + i)
           .setParents("InitialChoice")
           .setProbability(1, "T", "TT")
           .setProbability(0, "L", "TT")
@@ -40,30 +46,30 @@ public class ExampleTest extends TestCase {
     BayesNetwork network = getLadyOrTigerNetwork();
 
     // Compute the probability of surviving the first round.
-    Event queryEvent = Event.varEquals("Reveal1", "L");
+    Event queryEvent = varEquals("Reveal1", "L");
 
     double probability = network.queryProbability(queryEvent);
     assertEquals(1/2d, probability, DELTA);
 
     // Compute the probability of surviving all 3 rounds.
-    queryEvent = Event.and(ImmutableList.of(
-        Event.varEquals("Reveal1", "L"),
-        Event.varEquals("Reveal2", "L"),
-        Event.varEquals("Reveal3", "L")));
+    queryEvent = and(ImmutableList.of(
+        varEquals("Reveal1", "L"),
+        varEquals("Reveal2", "L"),
+        varEquals("Reveal3", "L")));
 
     probability = network.queryProbability(queryEvent);
     assertEquals(3/8d, probability, DELTA);
 
     // Compute the probability of picking the pair of doors with two tigers.
-    queryEvent = Event.varEquals("InitialChoice", "TT");
+    queryEvent = varEquals("InitialChoice", "TT");
 
     probability = network.queryProbability(queryEvent);
     assertEquals(1/3d, probability, DELTA);
 
     // Compute the probability of picking a pair of doors with at least one
     // tiger.
-    queryEvent = Event.or(
-        Event.varEquals("InitialChoice", "TT"), Event.varEquals("InitialChoice", "TL"));
+    queryEvent = or(
+        varEquals("InitialChoice", "TT"), varEquals("InitialChoice", "TL"));
 
     probability = network.queryProbability(queryEvent);
     assertEquals(2/3d, probability, DELTA);
@@ -77,25 +83,25 @@ public class ExampleTest extends TestCase {
 
     // Compute the probability of surviving all 3 rounds, given that the
     // protagonist has survived through two rounds so far.
-    Event queryEvent = Event.varEquals("Reveal3", "L");
-    Event evidence = Event.and(
-        Event.varEquals("Reveal1", "L"), Event.varEquals("Reveal2", "L"));
+    Event queryEvent = varEquals("Reveal3", "L");
+    Event evidence = and(
+        varEquals("Reveal1", "L"), varEquals("Reveal2", "L"));
 
     double probability = network.queryProbabilityWithEvidence(queryEvent, evidence);
     assertEquals(9/10d, probability, DELTA);
 
     // Should get the same result when the query event contains the evidence.
-    queryEvent = Event.and(ImmutableList.of(
-        Event.varEquals("Reveal1", "L"),
-        Event.varEquals("Reveal2", "L"),
-        Event.varEquals("Reveal3", "L")));
+    queryEvent = and(ImmutableList.of(
+        varEquals("Reveal1", "L"),
+        varEquals("Reveal2", "L"),
+        varEquals("Reveal3", "L")));
 
     probability = network.queryProbabilityWithEvidence(queryEvent, evidence);
     assertEquals(9/10d, probability, DELTA);
 
     // Compute the probability of surviving all 3 rounds, given that the
     // protagonist has survived through one round so far.
-    evidence = Event.varEquals("Reveal1", "L");
+    evidence = varEquals("Reveal1", "L");
 
     probability = network.queryProbabilityWithEvidence(queryEvent, evidence);
     assertEquals(3/4d, probability, DELTA);
@@ -107,16 +113,19 @@ public class ExampleTest extends TestCase {
    */
   private BayesNetwork getTramNetwork() {
     return BayesNetwork.builder()
-        .add(ConditionalDistribution.forVariable("Time")
+        .add(ConditionalDistribution
+            .forVariable("Time")
             .setProbability(1/3d, "Evening")
             .setProbability(2/3d, "Daytime")
             .build())
-        .add(ConditionalDistribution.forVariable("Line")
+        .add(ConditionalDistribution
+            .forVariable("Line")
             .setProbability(5/10d, "Line22")
             .setProbability(3/10d, "Line24")
             .setProbability(2/10d, "Line6")
             .build())
-        .add(ConditionalDistribution.forVariable("TramLength")
+        .add(ConditionalDistribution
+            .forVariable("TramLength")
             .setParents("Time", "Line")
             .setProbability(1/10d, "Short", "Evening", "Line22")
             .setProbability(9/10d, "Long", "Evening", "Line22")
@@ -131,7 +140,8 @@ public class ExampleTest extends TestCase {
             .setProbability(9/10d, "Short", "Daytime", "Line6")
             .setProbability(1/10d, "Long", "Daytime", "Line6")
             .build())
-        .add(ConditionalDistribution.forVariable("Direction")
+        .add(ConditionalDistribution
+            .forVariable("Direction")
             .setParents("Line")
             .setProbability(1/10d, "Albertov", "Line22")
             .setProbability(9/10d, "Pavlova", "Line22")
@@ -148,20 +158,61 @@ public class ExampleTest extends TestCase {
 
     // Compute the probability that a tram is going to Albertov, given
     // that it is evening and the tram is short.
-    Event queryEvent = Event.varEquals("Direction", "Albertov");
-    Event evidence = Event.and(
-        Event.varEquals("Time", "Evening"),
-        Event.varEquals("TramLength", "Short"));
+    Event queryEvent = varEquals("Direction", "Albertov");
+    Event evidence = and(
+        varEquals("Time", "Evening"),
+        varEquals("TramLength", "Short"));
 
     double probability = network.queryProbabilityWithEvidence(queryEvent, evidence);
     assertEquals(79/310d, probability, DELTA);
 
     // Compute the probability that a tram is long, given that it belongs
     // to Line 22.
-    queryEvent = Event.varEquals("TramLength", "Long");
-    evidence = Event.varEquals("Line", "Line22");
+    queryEvent = varEquals("TramLength", "Long");
+    evidence = varEquals("Line", "Line22");
 
     probability = network.queryProbabilityWithEvidence(queryEvent, evidence);
     assertEquals(29/30d, probability, DELTA);
+  }
+
+  /**
+   * Tests a simple network corresponding to the following example:
+   * http://en.wikipedia.org/wiki/Bayesian_network#Example
+   */
+  public void testGrassNetwork() {
+    BayesNetwork network = BayesNetwork.builder()
+        .add(ConditionalDistribution
+            .forVariable("Rain")
+            .setProbability(1/5d, "True")
+            .setProbability(4/5d, "False")
+            .build())
+        .add(ConditionalDistribution
+            .forVariable("Sprinkler")
+            .setParents("Rain")
+            .setProbability(2/5d, "True", "False")
+            .setProbability(3/5d, "False", "False")
+            .setProbability(1/100d, "True", "True")
+            .setProbability(99/100d, "False", "True")
+            .build())
+        .add(ConditionalDistribution
+            .forVariable("GrassWet")
+            .setParents("Sprinkler", "Rain")
+            .setProbability(0d, "True", "False", "False")
+            .setProbability(1d, "False", "False", "False")
+            .setProbability(4/5d, "True", "False", "True")
+            .setProbability(1/5d, "False", "False", "True")
+            .setProbability(9/10d, "True", "True", "False")
+            .setProbability(1/10d, "False", "True", "False")
+            .setProbability(99/100d, "True", "True", "True")
+            .setProbability(1/100d, "False", "True", "True")
+            .build())
+        .build();
+
+    Event queryEvent = varEquals("Rain", "True");
+    Event evidence = varEquals("GrassWet", "True");
+
+    double probability =
+        network.queryProbabilityWithEvidence(queryEvent, evidence);
+    assertEquals(891/2491d, probability, DELTA);
   }
 }
